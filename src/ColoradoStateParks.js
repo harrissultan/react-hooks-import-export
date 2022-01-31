d15da8e3d1ec104922d660e678303eddeabff11e
Harris Sultan <harris_sultan@outlook.com>
2022-01-31 13:55:56 -0500

    commit tree 9ea24c51b1978acfdb71aee4c90dfafd829d1b30

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,15 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerdePark from "./parks/MesaVerde";
+import * as RMFunctions from "./parks/RockyMountain";
 
-function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
-
-  return <h1>Colorado State Parks!</h1>;
+export default function ColoradoStateParks() {
+  return (
+    <div>
+{console.log(howManyParks())}
+      {console.log(RMFunctions.trees)}
+      {RMFunctions.wildlife()}
+      <MesaVerdePark />
+    </div>
+  );
 }
